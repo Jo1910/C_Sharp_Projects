f2e8a84e43503fa7b6ac304298fd3abb50584fc8
Jo1910 <jovanamitic@ymail.com>
2021-05-20 17:46:46 +0100

    Add Car Insurance App

---
+++ CarInsurance/CarInsurance/Controllers/AdminController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CarInsurance.Models;
+using CarInsurance.ViewModels;
+
+namespace CarInsurance.Controllers
+{
+    public class AdminController : Controller
+    {
+        
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (InsuranceEntities db = new InsuranceEntities())
+            {
+                var insurees = db.Insurees;
+                var insureeVms = new List<InsureeVm>();
+                foreach (var insuree in insurees)
+                {
+                    var insureeVm = new InsureeVm();
+                    insureeVm.FirstName = insuree.FirstName;
+                    insureeVm.LastName = insuree.LastName;
+                    insureeVm.EmailAddress = insuree.EmailAddress;
+                    insureeVm.Quote = Convert.ToInt32(insuree.Quote);
+
+                    insureeVms.Add(insureeVm);
+                }
+                return View(insureeVms);
+            }
+        }
+
+        // GET: Admin/Details/5
+    //    public ActionResult Details(int? id)
+    //    {
+    //        if (id == null)
+    //        {
+    //            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+    //        }
+    //        Insuree insuree = db.Insurees.Find(id);
+    //        if (insuree == null)
+    //        {
+    //            return HttpNotFound();
+    //        }
+    //        return View(insuree);
+    //    }
+
+    //    // GET: Admin/Create
+    //    public ActionResult Create()
+    //    {
+    //        return View();
+    //    }
+
+    //    // POST: Admin/Create
+    //    // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+    //    // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+    //    {
+    //        if (ModelState.IsValid)
+    //        {
+    //            db.Insurees.Add(insuree);
+    //            db.SaveChanges();
+    //            return RedirectToAction("Index");
+    //        }
+
+    //        return View(insuree);
+    //    }
+
+    //    // GET: Admin/Edit/5
+    //    public ActionResult Edit(int? id)
+    //    {
+    //        if (id == null)
+    //        {
+    //            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+    //        }
+    //        Insuree insuree = db.Insurees.Find(id);
+    //        if (insuree == null)
+    //        {
+    //            return HttpNotFound();
+    //        }
+    //        return View(insuree);
+    //    }
+
+    //    // POST: Admin/Edit/5
+    //    // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+    //    // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+    //    {
+    //        if (ModelState.IsValid)
+    //        {
+    //            db.Entry(insuree).State = EntityState.Modified;
+    //            db.SaveChanges();
+    //            return RedirectToAction("Index");
+    //        }
+    //        return View(insuree);
+    //    }
+
+    //    // GET: Admin/Delete/5
+    //    public ActionResult Delete(int? id)
+    //    {
+    //        if (id == null)
+    //        {
+    //            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+    //        }
+    //        Insuree insuree = db.Insurees.Find(id);
+    //        if (insuree == null)
+    //        {
+    //            return HttpNotFound();
+    //        }
+    //        return View(insuree);
+    //    }
+
+    //    // POST: Admin/Delete/5
+    //    [HttpPost, ActionName("Delete")]
+    //    [ValidateAntiForgeryToken]
+    //    public ActionResult DeleteConfirmed(int id)
+    //    {
+    //        Insuree insuree = db.Insurees.Find(id);
+    //        db.Insurees.Remove(insuree);
+    //        db.SaveChanges();
+    //        return RedirectToAction("Index");
+    //    }
+
+    //    protected override void Dispose(bool disposing)
+    //    {
+    //        if (disposing)
+    //        {
+    //            db.Dispose();
+    //        }
+    //        base.Dispose(disposing);
+    //    }
+    }
+}
---
+++ CarInsurance/CarInsurance/Controllers/HomeController.cs
@@ -0,0 +1,121 @@
+﻿using CarInsurance.Models;
+using CarInsurance.ViewModels;
+using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace CarInsurance.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult QuoteCalc(string FirstName, string LastName, string EmailAddress,
+                                      DateTime DateOfBirth, int CarYear, string CarMake, string CarModel,
+                                      int SpeedingTickets, bool CoverageType = false, bool DUI = false)
+        {
+                using (InsuranceEntities db = new InsuranceEntities())
+                {
+
+                    var insuree = new Insuree();
+                    insuree.FirstName = FirstName;
+                    insuree.LastName = LastName;
+                    insuree.EmailAddress = EmailAddress;
+                    insuree.DateOfBirth = DateOfBirth;
+                    insuree.CarYear = CarYear;
+                    insuree.CarMake = CarMake;
+                    insuree.CarModel = CarModel;
+                    insuree.DUI = DUI;
+                    insuree.CoverageType = CoverageType;
+                    insuree.SpeedingTickets = SpeedingTickets;
+                    insuree.Quote = TotalQuote(insuree);
+
+                    db.Insurees.Add(insuree);
+                    db.SaveChanges();
+
+                    return View(insuree);
+                }
+            }
+
+            private decimal TotalQuote(Insuree quote)
+            {
+
+                decimal quoteTotal = 50m;
+
+
+                int userBirth = Convert.ToDateTime(quote.DateOfBirth).Year;
+                int yearNow = DateTime.Now.Year;
+                int userAge = yearNow - userBirth;
+
+                // checking age
+
+                if (userAge <= 18)
+                {
+                    quoteTotal += 25;
+                }
+                else if (userAge >= 19 && userAge <= 25)
+                {
+                    quoteTotal += 50;
+                }
+                else if (userAge > 25)
+                {
+                    quoteTotal += 25;
+                }
+                // checking car year
+
+                if (quote.CarYear < 2000 || quote.CarYear > 2015)
+                {
+                    quoteTotal += 25;
+
+                }
+
+                //checking car make and model
+                if (quote.CarMake.ToLower() == "porsche")
+                {
+                    quoteTotal += 25;
+                    if (quote.CarModel.ToLower() == "911 carrera") quoteTotal += 25;
+
+                }
+
+                //check for speeding tickets
+                if (quote.SpeedingTickets > 0)
+                {
+                    quoteTotal = quoteTotal + (quote.SpeedingTickets * 10);
+
+                }
+
+                // check for DUI
+                if (quote.DUI == true)
+                {
+                    quoteTotal += quoteTotal / 4;
+
+                }
+                // chek for coverage
+                if (quote.CoverageType == true)
+                {
+                    quoteTotal += quoteTotal / 2;
+                }
+
+            decimal quoteTotal1 = Math.Round(quoteTotal, 2);
+            return quoteTotal1;
+            }
+
+        }
+    
+    }
+
+                
+                
+
+           
+
+        
